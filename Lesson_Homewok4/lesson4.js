608b93d0089184a93cfd2c9838acb3820c10aa40
RoksolanaK <meierella8@gmail.com>
2022-04-25 12:17:09 +0300

    lesson_hm4

---
+++ Lesson_Homewok4/lesson4.js
@@ -0,0 +1,24 @@
+// function test (first,secnd){
+//     if(typeof first === 'number' &&typeof secnd ==='number'){//щоб прирівняти до числа потрібно поставити typeof!!!
+//         console.log(first + secnd);
+//     } else {
+//         console.log('nothing');
+//     }
+// }
+// test(1,2) // 3
+// test(1, 'hello') //nothing
+//
+// function test2(a,b){
+//     return a * b;
+// }
+// test2(2,2)
+// console.log(test2(2,2));
+
+function test3 (a,s){
+    return a + s;
+}
+let useSolution = test3(1,1);
+console.log(useSolution +'hello')
+
+//питання на лекцію: щоб функція щось залишила нам, потрібно створити змінну?
+//чи є обмеження для створення назв аргументів функцій?
