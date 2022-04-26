cc5bc81ba596ae3f1f7f11b3147e4c3df6a82e05
RoksolanaK <meierella8@gmail.com>
2022-04-26 23:45:40 +0300

    lesson_hm4 corrected

--- Lesson_Homewok_4/homework4.js
+++ Lesson_Homewok_4/homework4.js
@@ -10,60 +10,77 @@
 // function circle (a){
 //     return 2 * Math.PI * a
 // }
-// circle(2)
+// console.log(circle(2));
 // document.write(`<h3>Площа кола - ${circle(2)}</h3>`)
 // //_________________________________________________________________________________________________
 // //створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
 // function cylinder (r, h){
 //     return 2 * Math.PI * r * h;
 // }
-// cylinder(2,5)
 // console.log(cylinder(2,5));
 // document.write(`<h3>Площа циліндра - ${cylinder(2,5)}</h3>`)
 //_____________________________________________________________________________________________________
 //створити функцію яка приймає масив та виводить кожен його елемент
 // First solution
 // let massif1 = [25,35,45,55,'hey','how are you?'];
-// function noName(){
+// function noName(array){
+//     for (let arrayElement of massif1) {
+//         document.write(`<ul><li>${arrayElement}</li></ul>`)
+//     }
 // }
-// let result = noName(console.log(massif1));
+// noName(console.log(massif1));
 
 //Second solution
-// function noName2(){
+// function noName1(array) {
+//     for (let arrayElement1 of array) {
+//         console.log(arrayElement1);
+//         document.write(`<ul><li>${arrayElement1}</li></ul>`)
+//     }
 // }
-// noName2(console.log([25,35,45,55,'hey','how are you?']))
-// WHICH ONE IS CORRECT????????????
+//
+// noName1([1, true, 'hello']);
+
+//Third solution
+// let massif2 = [25,35,45,55,'hey','how are you?'];
+// function noName(array){
+//     for (let arrayElement of massif2) {
+//         console.log(arrayElement);
+//         document.write(`<ul><li>${arrayElement}</li></ul>`)
+//     }
+// }
+// noName(massif2);
 //______________________________________________________________________________________________________
 // створити функцію яка створює параграф з текстом. Текст задати через аргумент
-// function text(){
-//    console.log(arguments);
+// function text(text){
+//     return text;
 // }
-// text('hello, this is a paragraph with some uninteresting text')
+// console.log(text('hello, this is a paragraph with some uninteresting text'));
 //__________________________________________________________________________________________________
 //створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-//function sun(){
-//     document.write(`<ul><li>Some texts</li></ul>`)
-//     document.write(`<ul><li>Some texts</li></ul>`)
-//     document.write(`<ul><li>Some texts</li></ul>`)
+// function paragraph (text1){
+//     document.write(`<ul><li>${text1}</li></ul>`)
+//     document.write(`<ul><li>${text1}</li></ul>`)
+//     document.write(`<ul><li>${text1}</li></ul>`)
 // }
-// sun();
+// paragraph('Hello! How do you do?');
 //__________________________________________________________________________________________________________
 //створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
 // Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
-// function liInUl (){
-//     for (let i = 0; i < 3; i++) {
-//        // console.log(i);
-//        document.write(`<ul><li>Some text 1</li></ul>`)
+// function liInUl (text3, a){
+//     for (let i = 0; i < a; i++) {
+//        console.log(text3);
+//        document.write(`<ul><li>${text3}</li></ul>`)
 //     }
 // }
-// liInUl()
+// liInUl('Goodbye! Take care!',5)
 //_________________________________________________________________________
 //створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 //
 // function niceFunc(array){
 //  for (let arrayElement of array) {
 //   document.write(`<ol><li>${arrayElement}</li></ol>`)
+//      console.log(arrayElement);
 //  }
 // }
 //   niceFunc([20,true, 'hello',false,43])
@@ -84,9 +101,23 @@
 // block(users)
 //_________________________________________________________________________________________________________________
 // створити функцію яка повертає найменьше число з масиву
+//first solution
 // function minNumber(){
 // }
 // minNumber(console.log(Math.min(100,200,20)))
+
+//second solution
+// let arr = [1,2,3,4,5,6];
+// function minNumb(array){
+//     let minN = array[0];
+//     for (let number of array) {
+//         if( number<minN){
+//             minN= number
+//         }
+//         return minN
+//     }
+// }
+// console.log(minNumb(arr));
 //_________________________________________________________________________________________________________
 //створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
 
